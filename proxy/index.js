a54964052e06d5ce66553d4ce8e80f138b46b982
Icer <liangshuangde@163.com>
2015-02-10 07:48:27 +0800

    fix missing proxy ref

--- proxy/index.js
+++ proxy/index.js
@@ -2,4 +2,5 @@ exports.User = require('./user');
 exports.Message = require('./message');
 exports.Topic = require('./topic');
 exports.Reply = require('./reply');
+exports.WxMsg = require('./wxmsg');
 exports.TopicCollect = require('./topic_collect');
